6e82c6d2f6e731a4c7c2bab97fc74bdce2a9c360
Rosinskyi <sergey22061993@gmail.com>
2019-08-21 19:44:25 +0300

    added field color to equals and hashcode

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -18,6 +18,18 @@ public class Rectangle {
         this.color = color;
     }
 
+    public Integer getWidth() {
+        return width;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -29,23 +41,12 @@ public class Rectangle {
 
         Rectangle rectangle = (Rectangle) o;
         return Objects.equals(width, rectangle.width)
-                && Objects.equals(length, rectangle.length);
+                && Objects.equals(length, rectangle.length)
+                && Objects.equals(color, rectangle.color);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(width, length);
-    }
-
-    public Integer getWidth() {
-        return width;
-    }
-
-    public Integer getLength() {
-        return length;
-    }
-
-    public String getColor() {
-        return color;
+        return Objects.hash(width, length, color);
     }
 }
