2087c9340e56a61640715c7219e5e0aa03df77a2
roman_minevich <283389dv3qsh>
2019-08-21 20:00:14 +0300

    Added solution

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Objects;
 
 /**
- * <Дан класс Rectangle. Переопределите в нем методы equals() и hashCode() так, чтобы equals()
+ * Дан класс Rectangle. Переопределите в нем методы equals() и hashCode() так, чтобы equals()
  * сравнивал экземпляры Rectangle по содержимому полей width и length,
  * а hashCode() был бы согласованным с реализацией equals().
  */
