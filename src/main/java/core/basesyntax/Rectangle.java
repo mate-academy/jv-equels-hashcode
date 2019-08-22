990e0c7cdfe6fd31a24749d430eaf1cd784336a5
Koval <xui>
2019-08-22 21:57:24 +0300

    Rect

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -16,6 +16,25 @@ public class Rectangle {
         this.color = color;
     }
 
+    @Override
+    public boolean equals(Object obj){
+        if (obj == this) {
+            return true;
+        }
+        if (obj instenceof Rectangle) {
+            Rectangle rec = (Rectangle) obj;
+        } else {
+            return false;
+        }
+        return this.getColor().equals(rec.getColor()) && this.getLength() == rec.getLength()
+                && this.getWidth() == rec.getWidth();
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * (this.getWidth() + this.getColor().length() + this.getLength());
+    }
+
     public Integer getWidth() {
         return width;
     }
