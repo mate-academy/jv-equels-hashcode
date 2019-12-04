a11ce786b50d88416396d2c0d16d02e0190284ed
Vadim Chaika <proxik777@gmail.com>
2019-12-05 00:10:05 +0200

    added a solution for equals and hashcode homework

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 /**
  * <p>Дан класс Rectangle. Переопределите в нем методы equals() и hashCode() так, чтобы equals()
  * сравнивал экземпляры Rectangle по содержимому полей color, width и length,
@@ -27,4 +29,32 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Rectangle rectangle = (Rectangle) obj;
+        if (!this.color.equals(rectangle.color)) {
+            return false;
+        }
+        if (!this.width.equals(rectangle.width)) {
+            return false;
+        }
+        if (!this.length.equals(rectangle.length)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(width, length, color);
+    }
 }
+
+
