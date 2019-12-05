c2257b75c097f90203e2967fc3a0b83909efa7bb
Serhii Blakh <s.a.blakh@ukr.net>
2019-12-05 11:48:58 +0200

    added solution homework_3.2

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -27,4 +27,35 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (obj instanceof Rectangle) {
+            Rectangle rectangle = (Rectangle) obj;
+            if (this.color.equals(rectangle.getColor())) {
+                return true;
+            }
+            if (this.length != rectangle.getLength()) {
+                return false;
+            }
+            if (this.width != rectangle.getWidth()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 0;
+        result = this.getLength() * 31 + this.getWidth()
+                + this.getColor().length() * this.color.hashCode();
+        return result;
+    }
 }
