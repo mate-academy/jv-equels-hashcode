56be0f1053efb717c8ba93a90f29a747a4abbe5e
Polina Kulyk <polina.kulyk@protonmail.com>
2020-04-03 15:45:36 +0300

    elininated objects usage and enhanced check for null

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 /**
  * <p>Дан класс Rectangle. Переопределите в нем методы equals() и hashCode() так, чтобы equals()
  * сравнивал экземпляры Rectangle по содержимому полей color, width и length,
@@ -18,38 +16,29 @@ public class Rectangle {
         this.color = color;
     }
 
-    public Integer getWidth() {
-        return width;
-    }
-
-    public Integer getLength() {
-        return length;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
     @Override
     public boolean equals(Object object) {
         if (this == object) {
             return true;
         }
-        if (object == null || getClass() != object.getClass()) {
+        if (object == null) {
             return false;
         }
-        Rectangle rectangle = (Rectangle) object;
-        return Objects.equals(width, rectangle.width)
-                && Objects.equals(length, rectangle.length)
-                && Objects.equals(color, rectangle.color);
+        if (object.getClass().equals(Rectangle.class)) {
+            Rectangle rectangle = (Rectangle) object;
+            return rectangle.width.equals(width)
+                    && rectangle.length.equals(length)
+                    && rectangle.color.equals(color);
+        }
+        return false;
     }
 
     @Override
     public int hashCode() {
         int result = 73;
-        result = width == null ? result : 57 * result + width.hashCode();
-        result = length == null ? result : 57 * result + length.hashCode();
-        result = color == null ? result : 57 * result + color.hashCode();
+        result = width != null ? 57 * result + width.hashCode() : result;
+        result = length != null ? 57 * result + length.hashCode() : result;
+        result = color != null ? 57 * result + color.hashCode() : result;
         return result;
     }
 }
