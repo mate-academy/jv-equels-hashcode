4c3ae459297b4aa66642afadc8aacc5b26c714d9
Vitamin-oht <61590116+Vitamin-oht@users.noreply.github.com>
2020-04-05 21:08:44 +0300

    Solution added

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -27,4 +27,28 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (color == null ? 0 : color.hashCode());
+        result = 31 * result + (width == null ? 0 : color.hashCode());
+        result = 31 * result + (color == null ? 0 : color.hashCode());
+        return result;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass().equals(getClass())) {
+            Rectangle rectangle = (Rectangle) obj;
+            return color.equals(rectangle.color)
+                    && width.equals(rectangle.width)
+                    && length.equals(rectangle.length);
+        }
+        return false;
+    }
 }
