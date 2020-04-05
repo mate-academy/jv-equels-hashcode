35cf36717e8fac0d02744362f47b4e72d2bda24f
Serhii Maksymov <serhio.maksymov@gmail.com>
2020-04-05 20:03:00 +0300

    added solution hw-6.2

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -27,4 +27,27 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null) {
+            return false;
+        }
+        if (object.getClass().equals(Rectangle.class)) {
+            Rectangle rectangle = (Rectangle) object;
+            return width.equals(rectangle.width)
+                    && length.equals(rectangle.length)
+                    && color.equals(rectangle.color);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return width == null || length == null || color == null ? 0
+                : 17 * (17 * (17 * 7 + width) + length) + color.hashCode();
+    }
 }
