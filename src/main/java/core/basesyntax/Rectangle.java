84aa640d93aa221911e4b8a34677a3ec4a450ca4
NikolayAbysov <nikolay.abysov@ukr.net>
2020-04-05 18:41:37 +0300

    Equals method switched from instanceOf to getClass

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -39,7 +39,7 @@ public class Rectangle {
 
     @Override
     public boolean equals(Object obj) {
-        return obj.getClass().equals(this.getClass())
+        return obj.getClass() == this.getClass()
                 && ((Rectangle) obj).getColor().equals(this.color)
                 && ((Rectangle) obj).getLength().equals(this.length)
                 && ((Rectangle) obj).getWidth().equals(this.width);
