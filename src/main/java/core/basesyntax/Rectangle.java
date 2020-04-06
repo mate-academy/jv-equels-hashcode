86636b37b3dd87de18f19cc7bdd6178b7206e61b
Володимир <matrix.vova@gmail.com>
2020-04-06 08:55:08 +0300

    added solution

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -16,15 +16,28 @@ public class Rectangle {
         this.color = color;
     }
 
-    public Integer getWidth() {
-        return width;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (o.getClass().equals(Rectangle.class)) {
+            Rectangle rectangle = (Rectangle) o;
+            return width.equals(rectangle.width)
+                    && length.equals(rectangle.length)
+                    && color.equals(rectangle.color);
+        }
+        return false;
     }
 
-    public Integer getLength() {
-        return length;
-    }
-
-    public String getColor() {
-        return color;
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + width.hashCode();
+        result = 31 * result + length.hashCode();
+        result = 31 * result + color.hashCode();
+        return result;
     }
 }
