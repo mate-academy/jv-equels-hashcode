7761d296e76854327826730cc824a22f0b32b8db
Ilya <ilya03449@gmail.com>
2020-08-03 19:17:48 +0300

    done

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -16,6 +16,35 @@ public class Rectangle {
         this.color = color;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || !obj.getClass().equals(Rectangle.class)) {
+            return false;
+        }
+        Rectangle rectangle = (Rectangle) obj;
+        return (length == null || rectangle.length == null)
+                ? length == rectangle.length :
+                length.equals(rectangle.length)
+                        && (width == null || rectangle.width == null)
+                        ? width == rectangle.width :
+                        width.equals(rectangle.width)
+                                && (color == null || rectangle.color == null)
+                                ? color == rectangle.color :
+                                color.equals(rectangle.color);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 49;
+        result = 13 * result + (color == null ? 0 : color.hashCode());
+        result = 13 * result + (width == null ? 0 : width.hashCode());
+        result = 13 * result + (length == null ? 0 : length.hashCode());
+        return result;
+    }
+
     public Integer getWidth() {
         return width;
     }
