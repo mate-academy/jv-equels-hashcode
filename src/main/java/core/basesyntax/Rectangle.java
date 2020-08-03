418e634b5345067727ab381d7050b2120f797f8b
Dubovaya <dubovaya.java@gmail.com>
2020-08-03 21:53:33 +0300

    solution 6.2

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,10 +1,5 @@
 package core.basesyntax;
 
-/**
- * <p>Дан класс Rectangle. Переопределите в нем методы equals() и hashCode() так, чтобы equals()
- * сравнивал экземпляры Rectangle по содержимому полей color, width и length,
- * а hashCode() был бы согласованным с реализацией equals().</p>
- */
 public class Rectangle {
     private Integer width;
     private Integer length;
@@ -26,5 +21,33 @@ public class Rectangle {
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 42;
+        int result = 1;
+        result = prime * result + (width == null ? 0 : width.hashCode());
+        result = prime * result + (color == null ? 0 : color.hashCode());
+        result = prime * result + (length == null ? 0 : length.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || !obj.getClass().equals(Rectangle.class)) {
+            return false;
+        }
+        Rectangle objectCast = (Rectangle) obj;
+        if (length == null && width == null && color == null && objectCast.color == null
+                && objectCast.width == null && objectCast.length == null) {
+            return hashCode() == objectCast.hashCode();
+        }
+        return (length != null && length.equals(objectCast.length))
+                && (width != null && width.equals(objectCast.width))
+                && (color != null && color.equals(objectCast.color));
     }
 }
