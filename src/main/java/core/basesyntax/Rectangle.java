68abf24b27a6da6aae6938a9ec5185a0fe3e958c
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-08-03 15:09:19 +0300

    added solution to hw6-2

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -27,4 +27,26 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other != null && getClass() == other.getClass() && this == other) {
+            return true;
+        }
+
+        Rectangle rect = (Rectangle) other;
+        return (width == rect.width || (width != null && width.equals(rect.getWidth())))
+                && (length == rect.length || (length != null && length.equals(rect.getLength())))
+                && (color == rect.color || (color != null && color.equals(rect.getColor())));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 42;
+        int result = 11;
+        result = prime * result + ((width != null) ? width.hashCode() : 0);
+        result = prime * result + ((length != null) ? length.hashCode() : 0);
+        result = prime * result + ((color != null) ? color.hashCode() : 0);
+        return result;
+    }
 }
