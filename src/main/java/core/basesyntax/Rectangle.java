9065dba56e1fa4f9422d1c701bfa2b013f47893f
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-08-03 13:56:30 +0300

    solution added

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,10 +1,5 @@
 package core.basesyntax;
 
-/**
- * <p>Дан класс Rectangle. Переопределите в нем методы equals() и hashCode() так, чтобы equals()
- * сравнивал экземпляры Rectangle по содержимому полей color, width и length,
- * а hashCode() был бы согласованным с реализацией equals().</p>
- */
 public class Rectangle {
     private Integer width;
     private Integer length;
@@ -17,6 +12,7 @@ public class Rectangle {
     }
 
     public Integer getWidth() {
+
         return width;
     }
 
@@ -26,5 +22,42 @@ public class Rectangle {
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Rectangle rectangle = (Rectangle) obj;
+        if ((width == rectangle.width) && (length != rectangle.length)
+                && (color.equals(rectangle.getColor()))) {
+            return true;
+        }
+
+        if ((width == null && rectangle.width == null)
+                && (length == null && rectangle.length == null)
+                && (color == null && rectangle.color == null)) {
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((width == null) ? 0 : width.hashCode());
+        result = prime * result + ((length == null) ? 0 : length.hashCode());
+        result = prime * result + ((color == null) ? 0 : color.hashCode());
+        return result;
     }
 }
