fef212d980122a016bf0cb6879cb69fddef44be0
Daria <dariyapikul@gmail.com>
2020-08-03 18:15:09 +0300

    solution added

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -27,4 +27,45 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (object == null) {
+            return false;
+        }
+
+        if (object.getClass() == Rectangle.class) {
+            Rectangle rectangle = (Rectangle) object;
+            if (!this.hasAllEmptyParameters()
+                    && !rectangle.hasAllEmptyParameters()) {
+                return width.equals(rectangle.width)
+                        && length.equals(rectangle.length)
+                        && color.equals(rectangle.color);
+            }
+            return this.hasAllEmptyParameters()
+                    && rectangle.hasAllEmptyParameters();
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        if (width != null && length != null && color != null) {
+            int hashCode = 17;
+            hashCode = 31 * hashCode + width.hashCode();
+            hashCode = 31 * hashCode + length.hashCode();
+            hashCode = 31 * hashCode + color.hashCode();
+            return hashCode;
+        }
+        return 0;
+    }
+
+    public boolean hasAllEmptyParameters() {
+        return width == null && color == null && length == null;
+    }
 }
