5eb8dc782af0e1562e06628c6214f6de037f5f3f
Artem Vremere <vremere_a@ukr.net>
2020-08-03 23:31:41 +0300

    added solution

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 /**
  * <p>Дан класс Rectangle. Переопределите в нем методы equals() и hashCode() так, чтобы equals()
  * сравнивал экземпляры Rectangle по содержимому полей color, width и length,
@@ -26,5 +28,36 @@ public class Rectangle {
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        final int prime = 31;
+        result = prime * result + Objects.hashCode(width);
+        result = prime * result + Objects.hashCode(length);
+        result = prime * result + Objects.hashCode(color);
+        ;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj instanceof Rectangle) {
+            Rectangle rectangle = (Rectangle) obj;
+            return Objects.equals(width, rectangle.width)
+                    && Objects.equals(length, rectangle.length)
+                    && Objects.equals(color, rectangle.color)
+                    && rectangle != null;
+        }
+        return false;
     }
 }
