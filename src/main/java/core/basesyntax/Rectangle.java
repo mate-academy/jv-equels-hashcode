ac8157967a9a3d1e51b51c2a765e7992353aa134
Your Name <i.dymytreyeva@gmail.com>
2020-08-03 15:49:56 +0300

    solution added

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -27,4 +27,33 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null) {
+            return false;
+        }
+        if (object.getClass().equals(Rectangle.class)) {
+            Rectangle rectangle = (Rectangle) object;
+            return (color != null && color.equals(rectangle.color))
+                    || (color == null && rectangle.color == null)
+                    && (width != null && width == rectangle.width)
+                    || (width == null && rectangle.width == null)
+                    && (length != null && length == rectangle.length)
+                    || (length == null && rectangle.length == null);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 14;
+        result = 28 * result + (color == null ? 0 : color.hashCode());
+        result = 28 * result + (width == null ? 0 : width);
+        result = 28 * result + (length == null ? 0 : length);
+        return result;
+    }
 }
