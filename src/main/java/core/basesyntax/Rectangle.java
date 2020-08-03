9359b33117e4e5a70953e12ac5216e092e76c988
Viktor Nikolaev <nikolaev.vikt@gmail.com>
2020-08-03 19:03:06 +0300

    [DEL] main from Rectangle class

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -82,25 +82,4 @@ public class Rectangle {
         }
         return result;
     }
-
-    public static void main(String[] args) {
-//        Rectangle rectangle1 = new Rectangle(12, 12, "olo");
-//        Rectangle rectangle2 = new Rectangle(12, 12, "olo");
-//        Rectangle rectangle3 = new Rectangle(12, 12, "alice");
-
-        Rectangle firstRectangle = new Rectangle(null, null, null);
-        Rectangle secondRectangle = new Rectangle(null, null, null);
-
-        System.out.println(firstRectangle.hashCode());
-        System.out.println(secondRectangle.hashCode());
-
-//        System.out.println(rectangle1.equals(rectangle2));
-//        System.out.println(rectangle2.equals(rectangle1));
-//
-//        System.out.println(rectangle1.hashCode());
-//        System.out.println(rectangle2.hashCode());
-//
-//        System.out.println(rectangle1.equals(rectangle3));
-//        System.out.println(rectangle3.hashCode());
-    }
 }
