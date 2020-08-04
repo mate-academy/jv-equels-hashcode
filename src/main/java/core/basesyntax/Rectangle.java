6fbed1f66c9f87a6cc6d8b1bf63021efcff13f7c
FYarkiy <flx_jb@yahoo.com>
2020-08-04 17:59:17 +0300

    shortern version of return

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -38,18 +38,12 @@ public class Rectangle {
 
         if (object.getClass().equals(Rectangle.class)) {
             Rectangle rectangle = (Rectangle) object;
-            return !((width == null && rectangle.width != null)
-                    || (width != null && rectangle.width == null)
-                    || (length == null && rectangle.width != null)
-                    || (length != null && rectangle.width == null)
-                    || (color == null && rectangle.color != null)
-                    || (color != null && rectangle.color == null))
-                    && (((width == null && rectangle.width == null)
-                    || width.equals(rectangle.width))
-                    && ((length == null && rectangle.width == null)
-                    || length.equals(rectangle.length))
-                    && ((color == null && rectangle.color == null)
-                    || (color.equals(rectangle.color))));
+            return (((width == null) && (rectangle.width == null))
+                    || (width != null && width.equals(rectangle.width)))
+                    && (((length == null) && (rectangle.length == null))
+                    || (length != null && length.equals(rectangle.length)))
+                    && (((color == null) && (rectangle.color == null))
+                    || (color != null && color.equals(rectangle.color)));
         }
         return false;
     }
