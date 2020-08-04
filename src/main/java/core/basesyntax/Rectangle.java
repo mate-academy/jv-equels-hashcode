bed500991aee847765e147e5e94ae9cc40b4f220
Victoria-Moiseienko <teasevic@gmail.com>
2020-08-04 16:16:20 +0300

    updated

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -43,10 +43,10 @@ public class Rectangle {
         if (object.getClass().equals(Rectangle.class)) {
             Rectangle rectangle = (Rectangle) object;
             return ((width == rectangle.getWidth())
-                            || (null != width && width.equals(rectangle.getWidth())))
+                            || (width != null && width.equals(rectangle.getWidth())))
                     &&
                     ((length == rectangle.getLength())
-                            || (null != length && length.equals(rectangle.getLength()))
+                            || (length != null && length.equals(rectangle.getLength()))
                     &&
                     color == rectangle.getColor());
         }
