aabfc2aa6bc623d5be8f11aed8ae5e5dcad9e6c5
jlightgreen <ShvabovichJulia@gmail.com>
2020-08-04 12:13:38 +0300

    changed parenthesis

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -37,12 +37,12 @@ public class Rectangle {
             return false;
         }
         Rectangle rectangle = (Rectangle) object;
-        return (width == null && rectangle.width == null)
-                || (width != null && width.equals(rectangle.width))
-                && (length == null && rectangle.length == null)
-                || (length != null && length.equals(rectangle.length))
-                && (color == null && rectangle.color == null)
-                || (color != null && color.equals(rectangle.color));
+        return (width == rectangle.width
+                || width != null && width.equals(rectangle.width))
+                && (length == rectangle.length
+                || length != null && length.equals(rectangle.length))
+                && (color == rectangle.color
+                || color != null && color.equals(rectangle.color));
     }
 
     @Override
