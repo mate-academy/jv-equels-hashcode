2145e973d37d14f43abd1d994e60a4daeb8c53dc
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-04 15:27:28 +0200

    done hw

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class Rectangle {
     private Integer width;
     private Integer length;
@@ -21,5 +23,34 @@ public class Rectangle {
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public boolean equals(Object rectangle) {
+        if (this == rectangle) {
+            return true;
+        }
+        if (rectangle == null) {
+            return  false;
+        }
+
+        if (rectangle.getClass().equals(Rectangle.class)) {
+            Rectangle current = (Rectangle) rectangle;
+            return (width == current.width || width.equals(current.width)) &&
+                    (length == current.length || length.equals(current.length)) &&
+                    (color == current.color || color.equals(current.color));
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 19;
+        result = 29 * result + (color == null ? 0 : color.hashCode());
+        result = 17 * result + (width == null ? 0 : width.hashCode());
+        result = 31 * result + (length == null ? 0 : length.hashCode());
+        return result;
+
     }
 }
