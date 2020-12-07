4606738440ebb6be3b787797dea4719985669f92
Alex <koleksandr4@gmail.com>
2020-12-07 14:41:42 +0200

    overrided equals and hashcode methods

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -22,4 +22,42 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object rectangle) {
+        Rectangle castedRectangle = (Rectangle) rectangle;
+        if (castedRectangle == this) {
+            return true;
+        }
+        if (castedRectangle == null) {
+            return false;
+        }
+        if (castedRectangle.getClass().equals(Rectangle.class)) {
+            if (width == null && castedRectangle.width != null) {
+                return false;
+            }
+            if (length == null && castedRectangle.length != null) {
+                return false;
+            }
+            if (color == null && castedRectangle.color != null) {
+                return false;
+            }
+            if (width == null && length == null && color == null) {
+                return true;
+            }
+            return this.width.equals(castedRectangle.width)
+                && this.length.equals(castedRectangle.length)
+                && this.color.equals(castedRectangle.color);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 5;
+        result = 7 * result + (width == null ? 0 : width.hashCode());
+        result = 7 * result + (length == null ? 0 : length.hashCode());
+        result = 7 * result + (color == null ? 0 : color.hashCode());
+        return result;
+    }
 }
