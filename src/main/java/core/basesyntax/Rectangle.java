095c3285418879d8ccc6f8f9559b3623bb60ad44
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-07 16:30:21 +0200

    first commit

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -22,4 +22,33 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Rectangle rectangle = (Rectangle) obj;
+        return (this.width == null && rectangle.width == null ? true :
+                this.width != null ? this.width.equals(rectangle.width) : false)
+                &&
+                (this.length == null && rectangle.length == null ? true :
+                        this.length != null ? this.length.equals(rectangle.length) : false)
+                &&
+                (this.color == null && rectangle.color == null ? true :
+                        this.color != null ? this.color.equals(rectangle.color) : false);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (this.width != null ? width : 0);
+        result = 31 * result + (this.length != null ? length : 0);
+        result = 31 * result + (this.color != null ? this.color.hashCode() : 0);
+        return result;
+    }
 }
