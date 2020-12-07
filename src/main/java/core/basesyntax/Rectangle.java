f48903eacd5552b03cc7ff8fa16d622e775d6883
Vitalik Kopach <apple_air@ukr.net>
2020-12-07 16:37:17 +0200

    implemented methods equals() and hashCode()

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class Rectangle {
     private Integer width;
     private Integer length;
@@ -21,5 +23,31 @@ public class Rectangle {
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public boolean equals(Object shape) {
+        if (shape == this) {
+            return true;
+        }
+        if (shape == null) {
+            return false;
+        }
+        if (shape.getClass().equals(Rectangle.class)) {
+            Rectangle current = (Rectangle) shape;
+            return Objects.equals(this.color, current.color)
+                    && Objects.equals(this.width, current.width)
+                    && Objects.equals(this.length, current.length);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (color == null ? 0 : color.hashCode());
+        result = 31 * result + (width == null ? 0 : width.hashCode());
+        result = 31 * result + (length == null ? 0 : length.hashCode());
+        return result;
     }
 }
