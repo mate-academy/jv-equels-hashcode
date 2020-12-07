7098dc31dd0098a406cd6eed910738282baa6f4e
ShemsheyMaxim <shanhaymax@gmail.com>
2020-12-07 18:33:19 +0200

    added override equals(), added override hashCode()

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -22,4 +22,33 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (width == null ? 0 : width.hashCode());
+        result = 31 * result + (length == null ? 0 : length.hashCode());
+        result = 31 * result + (color == null ? 0 : color.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object rectangle) {
+        if (this == rectangle) {
+            return true;
+        }
+        if (rectangle == null) {
+            return false;
+        }
+        if (rectangle.getClass().equals(Rectangle.class)) {
+            Rectangle current = (Rectangle) rectangle;
+            return (this.color == current.color
+                    || this.color != null && this.color.equals(current.color))
+                    && (this.length == current.length
+                    || this.length != null && this.length.equals(current.length))
+                    && (this.width == current.width
+                    || this.width != null && this.width.equals(current.width));
+        }
+        return false;
+    }
 }
