1a78c36098e82a9617b724c55e7ba4a079d489c4
kerrender <agreed_note@hotmail.com>
2020-12-07 22:52:21 +0200

    Initial

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -22,4 +22,27 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Rectangle current = (Rectangle) obj;
+        return (this.width == null || this.width.equals(current.width))
+                && (this.length == null || this.length.equals(current.length))
+                && (this.color == null || this.color.equals(current.color));
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 13;
+        result = 19 * result + (width == null ? 0 : width);
+        result = 19 * result + (length == null ? 0 : length);
+        result = 19 * result + (color == null ? 0 : color.hashCode());
+        return result;
+    }
 }
