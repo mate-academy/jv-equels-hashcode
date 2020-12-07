ee82ae910cb2e38daae16af5f1eb3daa5cb9d6fb
Igor Bochkor <igorbochkor@gmail.com>
2020-12-07 13:03:27 +0200

    added solution equals-hashcode

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -22,4 +22,36 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object rectangle) {
+        if (rectangle == null) {
+            return false;
+        }
+
+        if (this == rectangle) {
+            return true;
+        }
+
+        if (rectangle.getClass().equals(Rectangle.class)) {
+            Rectangle current = (Rectangle) rectangle;
+            return (this.width == current.width || this.width != null
+                    && this.width.equals(current.width))
+                    && (this.color == current.color || this.color != null
+                    && this.color.equals(current.color))
+                    && (this.length == current.length || this.length != null
+                    && this.length.equals(current.length));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 19;
+        result = 37 * (width == null ? 0 : width.hashCode());
+        result = 37 * (color == null ? 0 : color.hashCode());
+        result = 37 * (length == null ? 0 : length.hashCode());
+        return result;
+    }
 }
+
