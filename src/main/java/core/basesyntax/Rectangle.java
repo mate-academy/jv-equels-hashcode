4edea34079520d687de4e2654e20890e6566fa61
egorka <entertainment.veks@gmail.com>
2020-12-08 16:26:43 +0200

    yegor sereda first commit

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -22,4 +22,72 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object rectangle) {
+
+        Rectangle current = (Rectangle) rectangle;
+
+        if (rectangle == this) {
+            return true;
+        }
+
+        if (rectangle == null) {
+            return false;
+        }
+
+        if(rectangle.getClass().equals(Rectangle.class)) {
+            return colorCheck(current) && lengthCheck(current) && widthCheck(current);
+        }
+
+        return false;
+    }
+
+    private boolean colorCheck (Rectangle current) {
+        if (current.color == null) {
+            if (current.color == this.color) {
+                return true;
+            }
+        } else {
+            if (current.color.equals(this.color)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean lengthCheck (Rectangle current) {
+        if (current.length == null) {
+            if (current.length == this.length) {
+                return true;
+            }
+        } else {
+            if (current.length.equals(this.length)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean widthCheck (Rectangle current) {
+        if (current.width == null) {
+            if (current.width == this.width) {
+                return true;
+            }
+        } else {
+            if (current.width.equals(this.width)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (this.length == null ? 0 : this.length);
+        result = 31 * result + (this.width == null ? 0 : this.width);
+        result = 31 * result + (this.color == null ? 0 : this.color.hashCode());
+        return result;
+    }
 }
