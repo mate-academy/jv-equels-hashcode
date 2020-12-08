9368a4222d9226bcfe646180fc3b286124955f94
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-08 12:33:04 +0200

    changed hascode; equals

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 public class Rectangle {
     private Integer width;
     private Integer length;
@@ -35,9 +33,12 @@ public class Rectangle {
         }
         if (rectangle.getClass().equals(Rectangle.class)) {
             Rectangle current = (Rectangle) rectangle;
-            return Objects.equals(this.width, current.width)
-                  && Objects.equals(this.length, current.length)
-                  && Objects.equals(this.color, current.color);
+            return ((width == current.width
+                    || (width != null && width.equals(current.width)))
+                    && (length == current.length
+                    || (length != null && length.equals(current.length)))
+                    && (color == current.color
+                    || (color != null && color.equals(current.color))));
         }
         return false;
     }
@@ -45,9 +46,9 @@ public class Rectangle {
     @Override
     public int hashCode() {
         int hashCode = 17;
-        hashCode = 31 * hashCode + (width != null ? width.hashCode() : 0)
-                + (length != null ? length.hashCode() : 0)
-                + (color != null ? color.hashCode() : 0);
+        hashCode = 31 * hashCode + (width != null ? width.hashCode() : 0);
+        hashCode = 31 * hashCode + (length != null ? length.hashCode() : 0);
+        hashCode = 31 * hashCode + (color != null ? color.hashCode() : 0);
         return hashCode;
     }
 }
