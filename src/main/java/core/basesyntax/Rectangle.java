b03c775f38f7afbe63b9da80dc31105281161a4c
KostyantynProskura <kostyantyn.proskura@gmail.com>
2020-12-08 13:19:36 +0200

    1) ReCommit cause of travis

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -22,4 +22,34 @@ public class Rectangle {
     public String getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null) {
+            return false;
+        }
+        Rectangle rectangle = (Rectangle) object;
+        if ((this.color == null && rectangle.color == null)
+                && (this.length == null && rectangle.length == null)
+                && (this.width == null && rectangle.width == null)) {
+            return true;
+        } else if (object.getClass().equals(Rectangle.class)) {
+            return (this.color != null && this.color.equals(rectangle.color))
+                    && (this.length != null && this.length.equals(rectangle.length))
+                    && (this.width != null && this.width.equals(rectangle.width));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 11 * result + (color == null ? 0 : color.hashCode());
+        result = 11 * result + (width == null ? 0 : width.hashCode());
+        result = 11 * result + (length == null ? 0 : length.hashCode());
+        return result;
+    }
 }
