c6698bffd0d87a35fe8333c217adf29bbe329cd1
tbondare <tanya.bon2016@gmail.com>
2020-12-09 03:32:36 +0200

    solution was added firstly

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class Rectangle {
     private Integer width;
     private Integer length;
@@ -21,5 +23,28 @@ public class Rectangle {
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public boolean equals(Object rectangle) {
+        if (rectangle == null) {
+            return false;
+        }
+        Rectangle currentRectangle = (Rectangle) rectangle;
+        if (rectangle.getClass().equals(Rectangle.class)) {
+            return (Objects.equals(this.color, currentRectangle.color)
+                && Objects.equals(this.length, currentRectangle.length)
+                && Objects.equals(this.width, currentRectangle.width));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 11;
+        result = 19 * result + (color == null ? 0 : color.hashCode());
+        result = 21 + result + (length == null ? 0 : length.hashCode());
+        result = 29 * result + (width == null ? 0 : width.hashCode());
+        return result;
     }
 }
